fb6aeb94579805e09da947a5a46a5d60564be5fa
Shutovski Maxim <fortnaytus@bk.ru>
2026-07-10 14:38:02 +0700

    Now the triangle is colored.

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,23 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 Color;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "Color=aColor;\n"
     "}\0";
 
 const char *fragmentShaderSource="#version 330 core\n"
 "out vec4 FragColor;\n"
-
+"in vec3 Color;\n"
 "void main()\n"
 "{\n"
-"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"    FragColor = vec4(Color,1.0);\n"
 "}\0";
+
+
 
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
@@ -63,9 +68,9 @@ int main(void)
     unsigned int VBO,VAO;
     
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+        -0.5f, -0.5f, 0.0f,0.0f,0.2f,0.5f,
+        0.5f, -0.5f, 0.0f,0.125f,0.2f,0.125f,
+        0.0f,  0.5f, 0.0f,0.5f,0.0f,0.24f
     };
 
     int  success;
@@ -80,8 +85,11 @@ int main(void)
     
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(sizeof(float)*3));
+    glEnableVertexAttribArray(1);
 
     unsigned int FragmentShader,VertexShader;
 
