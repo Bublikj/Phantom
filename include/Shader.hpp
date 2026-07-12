e17ebc237772bb29c6517a1756d98fc9b83db911
Shutovski Maxim <fortnaytus@bk.ru>
2026-07-12 15:42:56 +0700

    Adding a new class "Shader",but the only declarated

---
+++ include/Shader.hpp
@@ -0,0 +1,22 @@
+#include <string>
+
+class Shader{
+public:
+    Shader(const char* PathGLSLShader);
+    Shader(const char* srcFragmentShader,const char* srcVertexShader);
+    Shader();
+    
+    void Setup(const char* PathGLSLShader);
+    void Setup(const char* srcFragmentShader,const char* srcVertexShader);
+    
+    void Clear();
+    
+    void setInt(const std::string &name,int value);
+    void setFloat(const std::string &name,float value);
+    void setBool(const std::string &name,bool value);
+    void setDouble(const std::string &name,double value);
+
+    unsigned int getID();
+private:
+    unsigned int ProgramId=0;
+};
