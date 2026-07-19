b5d434aa382975b896fff990902d252cb1ba2519
Shutovski Maxim <fortnaytus@bk.ru>
2026-07-19 14:08:54 +0700

    Fix bugs

--- include/Mesh.hpp
+++ include/Mesh.hpp
@@ -1,14 +1,13 @@
 #include "VertexArrayObject.hpp"
 #include "VertexBufferObject.hpp"
 #include "ElementBufferObject.hpp"
-namespace Phantom {
+
 class Mesh{
 
 public:
 
 private:
-    VertexArrayObject VAO;
-    VertexBufferObject VBO;
-    ElementBufferObject EBO;
+  Phantom::VertexArrayObject VAO;
+  Phantom::VertexBufferObject VBO;
+  Phantom::ElementBufferObject EBO;
 };
-}
